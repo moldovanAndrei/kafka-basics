5f7486106e0b2bacd5ec5327cb9080caaf78f354
moldoan <moldoan@emea.corpdir.net>
2022-12-19 22:36:24 +0200

    Reset consumer group ids

--- src/main/java/com/msg/demo/kafka/consumer/BatchKafkaConsumer.java
+++ src/main/java/com/msg/demo/kafka/consumer/BatchKafkaConsumer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class BatchKafkaConsumer {
 
-    //    @KafkaListener(topics = "string-topic", groupId = "batch_group_2")
+//    @KafkaListener(topics = "string-topic", groupId = "batch_group_1")
     public void listenStringTopicByList(ConsumerRecords<String, String> records) {
         System.out.println("Found " + records.count() + " records");
         records.forEach(record -> {
